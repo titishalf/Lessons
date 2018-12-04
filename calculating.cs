fa005cfb4c1f96439579c7787710c3d00666443f
titishalf <45240003+titishalf@users.noreply.github.com>
2018-12-04 15:21:45 +0200

    Added c#

---
+++ calculating.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class calculate
+    {
+        public double Add(double index1, double index2)
+        {
+            return index1 + index2;
+        }
+       
+        public double Divide(double index1, double index2)
+        {
+            if (index2 == 0)
+            {
+                throw new Exception("index 2 should not be equal to zero");
+            }
+            return index1 / index2;
+        }
+        public double Substract(double index1, double index2)
+        {
+            return index1 - index2;
+        }
+        public double Multiply(double index1, double index2)
+        {
+            return index1 * index2;
+        }
+    }
+}
---
+++ excuting program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int c;
+               var operation1 = new calculate();
+               Console.WriteLine("enter first index");
+               double index1 = Convert.ToDouble(Console.ReadLine());
+               Console.WriteLine("enter second index");
+               double index2 = Convert.ToDouble(Console.ReadLine());
+               Console.Write("choose any operator continue:\n\n");
+            Console.WriteLine("1. addition");
+            Console.WriteLine("2. Divide");
+            Console.WriteLine("3.substract ");
+            Console.WriteLine("4. Multiply\n\n\n\n\n");
+            int C = Convert.ToInt16(Console.ReadLine());
+            
+            if (C == 1)
+            {
+                operation1.Add(index1, index2);
+                Console.WriteLine("the result of :{0} and {1}is equal to {2}", index1, index2, operation1.Add(index1, index2));
+            }
+  else if( C==2)
+            {
+                operation1.Divide(index1, index2);
+                Console.WriteLine("the result of :{0} and {1}is equal to {2}", index1, index2, operation1.Divide(index1, index2));
+            }
+            else if (C == 3)
+            {
+                operation1.Substract(index1, index2);
+                Console.WriteLine("the result of :{0} and {1}is equal to {2}", index1, index2, operation1.substract(index1, nindex2));
+            }
+            else if (C == 4)
+            {
+                operation1.Multiply(index1, index2);
+                Console.WriteLine("the result of : {0} and {1}is equal to {2}", index1, index2, operation1.Multiply(index1, index2));
+            }
+            else
+            {
+                Console.WriteLine("impossible calculation");
+            }
+            Console.ReadKey();
+               
+
+         }
+
+    }
+}
